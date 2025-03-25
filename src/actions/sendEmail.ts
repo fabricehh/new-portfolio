7f73782d46d4604f962f336ba0fb71214ec5068e
Fabrice HOUESSOU <121553482+fabricehh@users.noreply.github.com>
2025-03-25 10:17:13 +0000

    Update sendEmail.ts

--- src/actions/sendEmail.ts
+++ src/actions/sendEmail.ts
@@ -1,39 +1,39 @@
-'use server'
-import { z } from 'zod';
-import nodemailer from 'nodemailer';
+// 'use server'
+// import { z } from 'zod';
+// import nodemailer from 'nodemailer';
 
-// Validation schema
-const emailSchema = z.object({
-  email: z.string().email(),
-  subject: z.string().min(3).optional(),
-  message: z.string().min(10),
-})
+// // Validation schema
+// const emailSchema = z.object({
+//   email: z.string().email(),
+//   subject: z.string().min(3).optional(),
+//   message: z.string().min(10),
+// })
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: parseInt(process.env.MAILTRAP_PORT || '2525'),
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAILTRAP_HOST,
+//   port: parseInt(process.env.MAILTRAP_PORT || '2525'),
+//   auth: {
+//     user: process.env.MAILTRAP_USER,
+//     pass: process.env.MAILTRAP_PASS,
+//   },
+// });
 
-export async function sendEmail(formData: FormData) {
-  try {
-    // Validate form data
-    const validatedFields = emailSchema.parse({
-      email: formData.get('email'),
-      subject: formData.get('subject'),
-      message: formData.get('message'),
-    })
-
-
-    console.log(validatedFields.email);
+// export async function sendEmail(formData: FormData) {
+//   try {
+//     // Validate form data
+//     const validatedFields = emailSchema.parse({
+//       email: formData.get('email'),
+//       subject: formData.get('subject'),
+//       message: formData.get('message'),
+//     })
 
 
-    return true
-  } catch (error) {
-    console.error('Failed to send email:', error)
-    return false
-  }
-}
+//     console.log(validatedFields.email);
+
+
+//     return true
+//   } catch (error) {
+//     console.error('Failed to send email:', error)
+//     return false
+//   }
+// }
